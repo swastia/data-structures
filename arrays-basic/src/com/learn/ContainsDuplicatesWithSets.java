806fb78ee5389ae1e13f80b69822b31d31f5a3e1
swastia <swasti.arya02gmail.com>
2022-08-08 01:01:53 +0530

    Program to find if an array contains duplicate numbers using sets

---
+++ arrays-basic/src/com/learn/ContainsDuplicatesWithSets.java
@@ -0,0 +1,24 @@
+package com.learn;
+
+import java.util.*;
+
+public class ContainsDuplicatesWithSets {
+
+    public static void main(String[] args) {
+        int[] input1 = new int[]{1,2,3,5,6,4};
+        boolean hasDuplicates = checkForDuplicates(input1);
+        System.out.println(hasDuplicates);
+    }
+
+    private static boolean checkForDuplicates(int[] input){
+        Set<Integer> set = new HashSet<>();
+
+        for(int i = 0; i<input.length; i++){
+            // put in hashset
+            if(!set.add(input[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+}
