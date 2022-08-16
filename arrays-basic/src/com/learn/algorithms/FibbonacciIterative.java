be3dc1efcfb4af3fa24eda7dedacdf084aff48f1
swastia <swasti.arya02gmail.com>
2022-08-16 14:49:49 +0530

    Fibonacci using Recursion and 2 approaches of iterative.

---
+++ arrays-basic/src/com/learn/algorithms/FibbonacciIterative.java
@@ -0,0 +1,46 @@
+package com.learn.algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FibbonacciIterative {
+
+    public static int fibbonacci2(int n){
+        // create initial index of list, based on these rest of the
+        List<Integer> fibbonacciList = new ArrayList<>();
+        fibbonacciList.add(0);
+        fibbonacciList.add(1);
+
+        for(int i = 2; i< n+1; i++){
+            fibbonacciList.add(fibbonacciList.get(i-2) + fibbonacciList.get(i-1));
+        }
+
+        return fibbonacciList.get(n);
+    }
+
+    public static int fibbonacci1(int n){
+        // return the number at n
+        int index1= 0;
+        int index2 = 1;
+        int nextIndex = 0;
+
+        for(int i = 2; i<=n ; i++){
+            nextIndex = index1 + index2;
+            index1 = index2;
+            index2 = nextIndex;
+        }
+        return nextIndex;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibbonacci2(3));
+        System.out.println(fibbonacci2(6));
+        System.out.println(fibbonacci2(4));
+        System.out.println(fibbonacci2(5));
+
+        System.out.println(fibbonacci1(3));
+        System.out.println(fibbonacci1(6));
+        System.out.println(fibbonacci1(4));
+        System.out.println(fibbonacci1(5));
+    }
+}
---
+++ arrays-basic/src/com/learn/algorithms/FibonacciRecursion.java
@@ -0,0 +1,31 @@
+package com.learn.algorithms;
+// Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+// the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 → 2+3
+
+//For example: fibonacciRecursive(6) should return 8
+
+public class FibonacciRecursion {
+
+    public static Integer fibonacci(int n){
+        //create the base case return
+
+        System.out.println("n is: " + n);
+        if(n < 2){
+            return n;
+        }
+
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(6));
+//        System.out.println(fibonacci(2));
+//        System.out.println(fibonacci(8));
+//        System.out.println(fibonacci(0));
+//        System.out.println(fibonacci(1));
+//        System.out.println(fibonacci(5));
+//        System.out.println(fibonacci(10));
+    }
+}
