374812bdead4290ea984d054a7690949b088ebcd
swastia <swasti.arya02gmail.com>
2022-08-19 11:45:17 +0530

    Bubble and Selection Sort

---
+++ arrays-basic/src/com/learn/algorithms/BubbleSort.java
@@ -0,0 +1,28 @@
+package com.learn.algorithms;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BubbleSort {
+
+    public List<Integer> sort(List<Integer> unsortedList){
+        //100, 20, 34, 54, 62, 0, 5, 1
+        for(int i=0; i < unsortedList.size(); i++ ){
+            for(int j=0; j < unsortedList.size()-1; j++){
+                int item1 = unsortedList.get(j);
+                int item2 = unsortedList.get(j+1);
+                if(item1 > item2){
+                    unsortedList.set(j, item2);
+                    unsortedList.set(j+1, item1);
+                }
+            }
+        }
+
+        return unsortedList;
+    }
+
+    public static void main(String[] args) {
+        BubbleSort bubbleSort = new BubbleSort();
+        System.out.println(bubbleSort.sort(Arrays.asList(100, 20, 34, 54, 62, 0, 5, 1)));
+    }
+}
---
+++ arrays-basic/src/com/learn/algorithms/SelectionSort.java
@@ -0,0 +1,27 @@
+package com.learn.algorithms;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SelectionSort {
+
+    public List<Integer> sort(List<Integer> unsortedList){
+        for(int i=0; i<unsortedList.size(); i++){
+            int smallest = i;
+            int currentItem = unsortedList.get(i);
+            for(int j=i+1; j < unsortedList.size(); j++){
+                if(unsortedList.get(smallest) > unsortedList.get(j)){
+                    smallest = j;
+                }
+            }
+            unsortedList.set(i, unsortedList.get(smallest));
+            unsortedList.set(smallest, currentItem);
+        }
+        return unsortedList;
+    }
+
+    public static void main(String[] args) {
+        SelectionSort selection = new SelectionSort();
+        System.out.println(selection.sort(Arrays.asList(100, 20, 200, 3, 8, 34, 54, 62, 0, 5, 1)));
+    }
+}
