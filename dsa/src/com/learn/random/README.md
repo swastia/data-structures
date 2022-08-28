99935d58f8476464772541868a034accee3cba90
swastia <swasti.arya02gmail.com>
2022-08-28 20:41:43 +0530

    Random Java questions

---
+++ dsa/src/com/learn/random/README.md
@@ -0,0 +1,3 @@
+This package contains random interview questions that I come across.
+- To verify my understanding, I shall practice and see the output here.
+- Package may contain errors, to highlight, why a certain code woldn't work.
---
+++ dsa/src/com/learn/random/StringAddRemovePrint.java
@@ -0,0 +1,20 @@
+package com.learn.random;
+
+import java.util.ArrayList;
+
+public class StringAddRemovePrint {
+
+    public static void main(String[] args) {
+
+        /*
+        * Method reference is used to refer method of functional interface.
+        * String is not functional interface
+        * */
+        var list = new ArrayList<String>();
+        list.add("Monday");
+        list.add(String::new);
+        list.add("Tuesday");
+        list.remove(0);
+        System.out.println(list.get(0));
+    }
+}
